eeb3343ce66df857dc408a48b784db99b2167d92
TulioHRC <tulio.prezotti@gmail.com>
2025-11-23 17:36:00 -0300

    feat: custom test

--- user/lottotest.c
+++ user/lottotest.c
@@ -3,20 +3,39 @@
 #include "user/user.h"
 #include "kernel/pstat.h"
 
-#define NUM_CHILDREN 3
-#define TICKET_RATIO_1 30
-#define TICKET_RATIO_2 30
-#define TICKET_RATIO_3 30
-#define LOOP_COUNT 10
+#define MAX_CHILDREN 10 
+#define LOOP_COUNT 1000
 #define PRINT_INTERVAL 1
 
 int main(int argc, char *argv[])
 {
-    int pids[NUM_CHILDREN];
-    int tickets[NUM_CHILDREN] = {TICKET_RATIO_1, TICKET_RATIO_2, TICKET_RATIO_3};
+    int num_children;
+    int tickets[MAX_CHILDREN];
+    int pids[MAX_CHILDREN];
+    int child_ticks[MAX_CHILDREN];
+
+    if (argc < 2) {
+        fprintf(2, "Usage: lottotest <ticket_ratio_1> [ticket_ratio_2 ...]\n");
+        exit(1);
+    }
+
+    num_children = argc - 1;
+    if (num_children > MAX_CHILDREN) {
+        fprintf(2, "Error: Maximum %d children supported.\n", MAX_CHILDREN);
+        exit(1);
+    }
+
+    for (int i = 0; i < num_children; i++) {
+        tickets[i] = atoi(argv[i + 1]);
+        if (tickets[i] <= 0) {
+            fprintf(2, "Error: Ticket ratios must be positive integers.\n");
+            exit(1);
+        }
+        child_ticks[i] = -1; // Initialize child_ticks here for dynamic use
+    }
 
     // Create child processes
-    for (int i = 0; i < NUM_CHILDREN; i++) {
+    for (int i = 0; i < num_children; i++) {
         pids[i] = fork();
         if (pids[i] == 0) {
             if (settickets(tickets[i]) < 0) {
@@ -32,7 +51,11 @@ int main(int argc, char *argv[])
     }
 
     // Parent process
-    printf("ticks_p1,ticks_p2,ticks_p3\n");
+    printf("[time] ");
+    for (int i = 0; i < num_children; i++) {
+        printf("ticks_p%d%s", i + 1, (i == num_children - 1) ? "" : ",");
+    }
+    printf("\n");
 
     for (int i = 0; i < LOOP_COUNT; i++) {
         if (i % PRINT_INTERVAL == 0) {
@@ -42,28 +65,44 @@ int main(int argc, char *argv[])
                 break;
             }
 
-            int child_ticks[NUM_CHILDREN] = {-1, -1, -1};
+            // Reset child_ticks for each print interval
+            for (int k = 0; k < num_children; k++) {
+                child_ticks[k] = -1;
+            }
+
             for (int j = 0; j < NPROC; j++) {
-                for (int k = 0; k < NUM_CHILDREN; k++) {
+                for (int k = 0; k < num_children; k++) {
                     if (ps.pid[j] == pids[k]) {
                         child_ticks[k] = ps.ticks[j];
                     }
                 }
             }
+            
+            int all_children_found = 1;
+            for(int k=0; k < num_children; k++) {
+                if(child_ticks[k] == -1) {
+                    all_children_found = 0;
+                    break;
+                }
+            }
 
-            if (child_ticks[0] != -1 && child_ticks[1] != -1 && child_ticks[2] != -1) {
-                printf("[%d] %d,%d,%d\n", i, child_ticks[0], child_ticks[1], child_ticks[2]);
+            if (all_children_found) {
+                printf("[%d] ", i);
+                for (int k = 0; k < num_children; k++) {
+                    printf("%d%s", child_ticks[k], (k == num_children - 1) ? "" : ",");
+                }
+                printf("\n");
             }
         }
         pause(1);
     }
 
-    for (int i = 0; i < NUM_CHILDREN; i++) {
+    for (int i = 0; i < num_children; i++) {
         kill(pids[i]);
     }
 
     // Wait for child processes to exit
-    for (int i = 0; i < NUM_CHILDREN; i++) {
+    for (int i = 0; i < num_children; i++) {
         wait(0);
     }
 
